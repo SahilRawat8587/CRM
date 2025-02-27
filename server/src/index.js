2d4b8614d4146e4bcc1019debb7bf12de5327594
Sahil Rawat <sahilr8587@gmail.com>
2025-02-27 14:48:01 +0530

    fix: update CORS origin to use production URL

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://crm-78zr.onrender.com',
     credentials: true
 }))
 
